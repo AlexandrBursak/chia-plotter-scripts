616a84beb2d0649889ef3014d4cf42daca91c091
AlexandrBursak <alex.bursak@datarobot.com>
2023-09-15 13:15:35 +0300

    Added plot size C6

--- calculate.py
+++ calculate.py
@@ -1,9 +1,17 @@
 import math
 
-plot_size_5=85195312
-plot_size_7=81816406
+plot_size = {
+	'C5': 85195312, # round to bigger size
+	'C6': 83486720, # round to bigger size
+	'C7': 81816406, # round to bigger size
+}
 
-merge=plot_size_5-plot_size_7
+bigger_plots = 'C5'
+smaller_plots = 'C7'
+smaller, bigger = plot_size[smaller_plots], plot_size[bigger_plots]
+
+
+merge=bigger-smaller
 print('Merge between sizes plots:', merge)
 
 disk_sizes = {
@@ -17,9 +25,9 @@ disk_sizes = {
 
 
 def get_total_space_use(disk_size, count_of_bigger, count_of_less):
-	total_space_use = count_of_bigger * plot_size_5 + count_of_less * plot_size_7
+	total_space_use = count_of_bigger * bigger + count_of_less * smaller
 
-	if disk_size-total_space_use > plot_size_7:
+	if disk_size-total_space_use > smaller:
 		count_of_less = count_of_less + 1
 		total_space_use, count_of_bigger, count_of_less = get_total_space_use(disk_size, count_of_bigger, count_of_less)
 
@@ -29,8 +37,8 @@ for size in disk_sizes:
 	disk_size=disk_sizes[size]
 	print('----- Disk size:', size, '==', disk_size, '-----')
 
-	count_of_bigger = math.floor(disk_size/plot_size_5)
-	total_use = count_of_bigger * plot_size_5
+	count_of_bigger = math.floor(disk_size/bigger)
+	total_use = count_of_bigger * bigger
 	free = disk_size - total_use
 	count_of_less = 0
 
@@ -41,13 +49,13 @@ for size in disk_sizes:
 		total_use, count_of_bigger, count_of_less = get_total_space_use(disk_size, count_of_bigger, count_of_less)
 		free = disk_size - total_use
 
-	validate_size = count_of_bigger * plot_size_5 + count_of_less * plot_size_7
+	validate_size = count_of_bigger * bigger + count_of_less * smaller
 
 	if validate_size > disk_size:
 		print('Something wrong', validate_size, '>', disk_size)
 	else:
 		print('Nice! Used space < total space ==', validate_size, '<', disk_size, ' Free space ±', free)
-		print('For', size, 'C5:', count_of_bigger, 'C7:', count_of_less)
+		print('For', size, bigger_plots, ':', count_of_bigger, smaller_plots, ':', count_of_less)
 	print('======= Cycle end =======')
 
 # Merge between sizes plots: 3378906
