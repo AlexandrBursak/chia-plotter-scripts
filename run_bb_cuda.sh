4d737eaeaa165d3ea908bbc910514e93a6207740
AlexandrBursak <alex.bursak@datarobot.com>
2023-10-05 15:05:29 +0300

    Refactored the run_bb_cuda script, store console log to file; removed odd code in the validate and coping

--- run_bb_cuda.sh
+++ run_bb_cuda.sh
@@ -8,11 +8,16 @@ fast_disk_dir="/mnt/Fast-disk/chia-plotter/plotting/"
 path_to_bladebit_cuda="/home/burik/plotter/bladebit_cuda/bladebit_cuda"
 path_to_bladebit="/home/burik/plotter/bladebit_cuda/bladebit_cuda"
 
+logs_file=""
+path_logs="plotting_logs"
+
 # set all core/threads of processor
 number_threads=$(nproc --all)
 
 # Folders, where should be stored the plots
-path_array=("/mnt/InPl14Tb-2/Plots" "/mnt/InPl12Tb/Plots")
+# path_array=("/mnt/InPl14Tb-2/Plots" "/mnt/InPl12Tb/Plots")
+path_array=( $fast_disk_dir )
+
 
 # Check for free space for plots
 get_free_space() {
@@ -30,6 +35,15 @@ get_free_space() {
 }
 
 plot_size=85195312 # for compress type 5
+# plot_size = {
+#     'C1': 91750400, # 87.5
+#     'C2': 90177536, # 86.0
+#     'C3': 88604672, # 84.5
+#     'C4': 86926951, # 82.9
+#     'C5': 85161440, # 81.3
+#     'C6': 83486720, # 79.6
+#     'C7': 81794364, # 78.0
+# }
 
 type_plotting="cudaplot"
 number_plots=200 # number plats that we want to plot
@@ -38,6 +52,14 @@ counter=1
 stop_plotting=false # for breaking the loop if free space is not enough
 while [ $counter -le $number_plots ]
 do
+    start_time=$(date +%s)
+
+    echo "---------- Cycle #$counter --- Date start: $(date -d @$start_time) -----------"
+
+    # Create a log file
+    logs_file="$path_logs/$start_time-$counter.log"
+    echo "Create log file $logs_file"
+    touch "$logs_file"
 
     # Check where is anough free space
     for current_path in ${path_array[@]}; do
@@ -56,8 +78,6 @@ do
         break
     fi
 
-    echo $counter
-
 # run the blidebit script
 if [[ "$type_plotting" == 'cudaplot' ]]; then
 $path_to_bladebit_cuda \
@@ -68,32 +88,63 @@ $path_to_bladebit_cuda \
     -n 1 \
     $type_plotting \
     $fast_disk_dir \
+    | tee "$logs_file" 2>&1 \
     /
 elif [[ "$type_plotting" == 'ramplot' ]]; then
 $path_to_bladebit \
     -t $number_threads \
     -f $farm_key \
     -c $pool_contract_address \
-    -z $compress \
+    --compress $compress \
     -n 1 \
     $type_plotting \
+    --check 100 --check-threshold 0.9 \
     $fast_disk_dir \
+    | tee "$logs_file" 2>&1 \
     /
 else
 $path_to_bladebit \
     -t $number_threads \
     -f $farm_key \
     -c $pool_contract_address \
-    -z $compress \
+    --compress $compress \
     -n 1 \
     -t1 <temp_directory> \
     $type_plotting \
+    --check 100 --check-threshold 0.9 \
     $fast_disk_dir \
+    | tee "$logs_file" 2>&1 \
     /
 fi
 
     ((counter++))
+    end_time=$(date +%s)
+    elapsed_time=$((end_time - start_time))
 
-    sleep 60
+    echo "Build one more Plot in $elapsed_time sec."
+    echo "Paused to 10s"
+    sleep 10
 done
 echo All done
+
+
+# ./helloerror
+# + file: no message; console: message 1,2,3;
+
+# ./helloerror >error.txt
+# + file: message 1,2; console: message 3;
+
+# ./helloerror 2>&1 >error.txt
+# + file: message 1,2; console: message 3;
+# + same as ./helloerror >error.txt
+
+# ./helloerror >error.txt 2>&1
+# + file: message 3,1,2; console: no message;
+# + note the order 3 is first, then 1, then 2
+
+# ./helloerror | tee error.txt 2>&1
+# + file: message 1,2; console: message 3,1,2;
+# + note the order 3 is first, then 1, then 2
+
+# ./helloerror 2>&1 | tee error.txt
+# + file: message 3,1,2; console: message 3,1,2;
